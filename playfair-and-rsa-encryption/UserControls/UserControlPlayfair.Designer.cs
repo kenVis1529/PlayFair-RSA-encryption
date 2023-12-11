cb2bab34f398eeef9491509f6b1c9238e1a09189
Minh Dang <minhdang16012003@gmail.com>
2023-12-11 08:18:10 +0700

    Playfair Encrypt and Decrypt

--- playfair-and-rsa-encryption/UserControls/UserControlPlayfair.Designer.cs
+++ playfair-and-rsa-encryption/UserControls/UserControlPlayfair.Designer.cs
@@ -29,25 +29,144 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            lbKey = new Label();
+            tbKey = new TextBox();
+            lbPlainText = new Label();
+            rtbPlain = new RichTextBox();
+            groupBox1 = new GroupBox();
+            rtbTable = new RichTextBox();
+            lbEncrypted = new Label();
+            rtbEncrypted = new RichTextBox();
+            btnEncrypt = new Button();
+            btnDecrypt = new Button();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(395, 277);
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(384, 22);
             label1.Name = "label1";
-            label1.Size = new Size(280, 41);
+            label1.Size = new Size(290, 41);
             label1.TabIndex = 0;
             label1.Text = "PlayFair Encryption";
+            // 
+            // lbKey
+            // 
+            lbKey.AutoSize = true;
+            lbKey.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            lbKey.Location = new Point(27, 81);
+            lbKey.Name = "lbKey";
+            lbKey.Size = new Size(38, 23);
+            lbKey.TabIndex = 1;
+            lbKey.Text = "Key";
+            // 
+            // tbKey
+            // 
+            tbKey.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            tbKey.Location = new Point(27, 107);
+            tbKey.Name = "tbKey";
+            tbKey.Size = new Size(432, 30);
+            tbKey.TabIndex = 2;
+            // 
+            // lbPlainText
+            // 
+            lbPlainText.AutoSize = true;
+            lbPlainText.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            lbPlainText.Location = new Point(27, 180);
+            lbPlainText.Name = "lbPlainText";
+            lbPlainText.Size = new Size(82, 23);
+            lbPlainText.TabIndex = 3;
+            lbPlainText.Text = "Plain text";
+            // 
+            // rtbPlain
+            // 
+            rtbPlain.Location = new Point(27, 206);
+            rtbPlain.Name = "rtbPlain";
+            rtbPlain.Size = new Size(432, 104);
+            rtbPlain.TabIndex = 4;
+            rtbPlain.Text = "";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(rtbTable);
+            groupBox1.Location = new Point(616, 81);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(403, 269);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Key Matrix";
+            // 
+            // rtbTable
+            // 
+            rtbTable.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            rtbTable.Location = new Point(27, 28);
+            rtbTable.Name = "rtbTable";
+            rtbTable.ReadOnly = true;
+            rtbTable.Size = new Size(347, 212);
+            rtbTable.TabIndex = 10;
+            rtbTable.Text = "";
+            // 
+            // lbEncrypted
+            // 
+            lbEncrypted.AutoSize = true;
+            lbEncrypted.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            lbEncrypted.Location = new Point(27, 327);
+            lbEncrypted.Name = "lbEncrypted";
+            lbEncrypted.Size = new Size(266, 23);
+            lbEncrypted.TabIndex = 6;
+            lbEncrypted.Text = "Encrypted Text or Decrypted Text";
+            // 
+            // rtbEncrypted
+            // 
+            rtbEncrypted.Location = new Point(27, 353);
+            rtbEncrypted.Name = "rtbEncrypted";
+            rtbEncrypted.Size = new Size(432, 104);
+            rtbEncrypted.TabIndex = 7;
+            rtbEncrypted.Text = "";
+            // 
+            // btnEncrypt
+            // 
+            btnEncrypt.BackColor = Color.FromArgb(0, 48, 73);
+            btnEncrypt.ForeColor = SystemColors.ButtonHighlight;
+            btnEncrypt.Location = new Point(27, 488);
+            btnEncrypt.Name = "btnEncrypt";
+            btnEncrypt.Size = new Size(120, 40);
+            btnEncrypt.TabIndex = 8;
+            btnEncrypt.Text = "Encrypt";
+            btnEncrypt.UseVisualStyleBackColor = false;
+            btnEncrypt.Click += btnEncrypt_Click;
+            // 
+            // btnDecrypt
+            // 
+            btnDecrypt.BackColor = Color.FromArgb(0, 48, 73);
+            btnDecrypt.ForeColor = SystemColors.ButtonHighlight;
+            btnDecrypt.Location = new Point(195, 488);
+            btnDecrypt.Name = "btnDecrypt";
+            btnDecrypt.Size = new Size(120, 40);
+            btnDecrypt.TabIndex = 9;
+            btnDecrypt.Text = "Decrypt";
+            btnDecrypt.UseVisualStyleBackColor = false;
+            btnDecrypt.Click += btnDecrypt_Click;
             // 
             // UserControlPlayfair
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btnDecrypt);
+            Controls.Add(btnEncrypt);
+            Controls.Add(rtbEncrypted);
+            Controls.Add(lbEncrypted);
+            Controls.Add(groupBox1);
+            Controls.Add(rtbPlain);
+            Controls.Add(lbPlainText);
+            Controls.Add(tbKey);
+            Controls.Add(lbKey);
             Controls.Add(label1);
             Name = "UserControlPlayfair";
             Size = new Size(1060, 600);
+            groupBox1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -55,5 +174,15 @@
         #endregion
 
         private Label label1;
+        private Label lbKey;
+        private TextBox tbKey;
+        private Label lbPlainText;
+        private RichTextBox rtbPlain;
+        private GroupBox groupBox1;
+        private Label lbEncrypted;
+        private RichTextBox rtbEncrypted;
+        private Button btnEncrypt;
+        private Button btnDecrypt;
+        private RichTextBox rtbTable;
     }
 }
--- playfair-and-rsa-encryption/UserControls/UserControlPlayfair.cs
+++ playfair-and-rsa-encryption/UserControls/UserControlPlayfair.cs
@@ -16,5 +16,276 @@ namespace playfair_and_rsa_encryption.UserControls
         {
             InitializeComponent();
         }
+        const int SIZE = 30;
+
+        //Convert the plain text to lowercase
+        static void ToLowerCase(char[] plain, int ps)
+        {
+            for (int i = 0; i < ps; i++)
+            {
+                if (plain[i] > 64 && plain[i] < 91)
+                    plain[i] += (char)32;
+            }
+        }
+
+        //Remove all spaces in a string
+        static int RemoveSpace(ref char[] plain, int ps)
+        {
+            char[] result = new char[ps];
+            int count = 0;
+
+            for (int i = 0; i < ps; i++)
+            {
+                if (plain[i] != ' ')
+                    result[count++] = plain[i];
+            }
+
+            result.CopyTo(plain, 0);
+            return count;
+        }
+        //Generate the key square
+        static void generateKeyTable(char[] key, int ks, char[,] keyT)
+        {
+            int i, j, k, flag = 0;
+            int[] dicty = new int[26];
+
+            for (i = 0; i < ks; i++)
+            {
+                if (key[i] != 'j')
+                    dicty[key[i] - 97] = 2;
+            }
+
+            dicty['j' - 97] = 1;
+
+            i = 0;
+            j = 0;
+
+            for (k = 0; k < ks; k++)
+            {
+                if (dicty[key[k] - 97] == 2)
+                {
+                    dicty[key[k] - 97] -= 1;
+                    keyT[i, j] = key[k];
+                    j++;
+                    if (j == 5)
+                    {
+                        i++;
+                        j = 0;
+                    }
+                }
+            }
+
+            for (k = 0; k < 26; k++)
+            {
+                if (dicty[k] == 0)
+                {
+                    keyT[i, j] = (char)(k + 97);
+                    j++;
+                    if (j == 5)
+                    {
+                        i++;
+                        j = 0;
+                    }
+                }
+            }
+        }
+
+        //Search for the characters of a digraph
+        //in the key square and return their position
+        static void Search(char[,] keyT, char a, char b, int[] arr)
+        {
+            int i, j;
+
+            if (a == 'j')
+                a = 'i';
+            else if (b == 'j')
+                b = 'i';
+
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    if (keyT[i, j] == a)
+                    {
+                        arr[0] = i;
+                        arr[1] = j;
+                    }
+                    else if (keyT[i, j] == b)
+                    {
+                        arr[2] = i;
+                        arr[3] = j;
+                    }
+                }
+            }
+        }
+
+        //Find the modulis with number
+        static int Mod5(int a)
+        {
+            return (a % 5);
+        }
+
+        static int Mod5v2(int a)
+        {
+            if (a < 0)
+                a += 5;
+            return (a % 5);
+        }
+
+        //Make the plain text length to be even
+        static int Prepare(char[] str, int ptrs)
+        {
+            if (ptrs % 2 != 0)
+            {
+                str[ptrs++] = 'x';
+            }
+            return ptrs;
+        }
+
+        static void Encrypt(char[] str, char[,] keyT, int ps)
+        {
+            int[] a = new int[4];
+
+            for (int i = 0; i < ps; i += 2)
+            {
+                Search(keyT, str[i], str[i + 1], a);
+
+                if (a[0] == a[2])
+                {
+                    str[i] = keyT[a[0], Mod5(a[1] + 1)];
+                    str[i + 1] = keyT[a[0], Mod5(a[3] + 1)];
+                }
+                else if (a[1] == a[3])
+                {
+                    str[i] = keyT[Mod5(a[0] + 1), a[1]];
+                    str[i + 1] = keyT[Mod5(a[2] + 1), a[1]];
+                }
+                else
+                {
+                    str[i] = keyT[a[0], a[3]];
+                    str[i + 1] = keyT[a[2], a[1]];
+                }
+
+            }
+        }
+
+        static void Decrypt(char[] str, char[,] keyT, int ps)
+        {
+            int[] a = new int[4];
+            for (int i = 0; i < ps; i += 2)
+            {
+                Search(keyT, str[i], str[i + 1], a);
+                if (a[0] == a[2])
+                {
+                    str[i] = keyT[a[0], Mod5v2(a[1] - 1)];
+                    str[i + 1] = keyT[a[0], Mod5v2(a[3] - 1)];
+                }
+                else if (a[1] == a[3])
+                {
+                    str[i] = keyT[Mod5v2(a[0] - 1), a[1]];
+                    str[i + 1] = keyT[Mod5v2(a[2] - 1), a[1]];
+                }
+                else
+                {
+                    str[i] = keyT[a[0], a[3]];
+                    str[i + 1] = keyT[a[2], a[1]];
+                }
+            }
+        }
+
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            lbEncrypted.Text = "Encrypted text";
+            char[] str = rtbPlain.Text.ToCharArray();
+            char[] key = tbKey.Text.ToCharArray();
+            int ps, ks;
+            char[,] keyT = new char[5, 5];
+
+            //Key
+            ks = key.Length;
+            ks = RemoveSpace(ref key, ks);
+            ToLowerCase(key, ks);
+
+            //PlainText
+            ps = str.Length;
+            ps = RemoveSpace(ref str, ps);
+            ToLowerCase(str, ps);
+
+            ps = Prepare(str, ps);
+
+            generateKeyTable(key, ks, keyT);
+
+
+            //Display Key matrix
+            int cellWidth = 8;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    string value = keyT[i, j].ToString();
+                    int padding = cellWidth - value.Length;
+                    string paddedValue = value.PadLeft(value.Length + padding / 2).PadRight(cellWidth);
+                    rtbTable.AppendText(paddedValue);
+                }
+                rtbTable.AppendText(Environment.NewLine);
+            }
+
+            Encrypt(str, keyT, ps);
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                rtbEncrypted.AppendText(str[i].ToString());
+                if ((i + 1) % 2 == 0 && i != str.Length - 1)
+                    rtbEncrypted.AppendText(" ");
+
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            lbEncrypted.Text = "Decrypted text";
+            char[] str = rtbPlain.Text.ToCharArray();
+            char[] key = tbKey.Text.ToCharArray();
+            int ps, ks;
+            char[,] keyT = new char[5, 5];
+
+            //Key
+            ks = key.Length;
+            ks = RemoveSpace(ref key, ks);
+            ToLowerCase(key, ks);
+
+            //PlainText
+            ps = str.Length;
+            ps = RemoveSpace(ref str, ps);
+            ToLowerCase(str, ps);
+
+            generateKeyTable(key, ks, keyT);
+
+
+            //Display Key matrix
+            int cellWidth = 8;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    string value = keyT[i, j].ToString();
+                    int padding = cellWidth - value.Length;
+                    string paddedValue = value.PadLeft(value.Length + padding / 2).PadRight(cellWidth);
+                    rtbTable.AppendText(paddedValue);
+                }
+                rtbTable.AppendText(Environment.NewLine);
+            }
+
+            Decrypt(str, keyT, ps);
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                rtbEncrypted.AppendText(str[i].ToString());
+                if ((i + 1) % 2 == 0 && i != str.Length - 1)
+                    rtbEncrypted.AppendText(" ");
+
+            }
+        }
     }
 }
